e6143eedec0d30617abff5178dff365e41c9d81b
Shubham Vasaikar <vasaikar@pdx.edu>
2018-12-04 21:27:36 -0800

    Remove redundant Home Component

--- frontend/src/component/SideLinkHeaders.js
+++ frontend/src/component/SideLinkHeaders.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
 
 const Header = () => (
     <header>
       <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/generateInv'>Generate Invoice</Link></li>
-          <li><Link to='/invoices'>View Invoice</Link></li>
-          <li><Link to='/clients'>Clients</Link></li>
-          <li><Link to='/products'>Products</Link></li>
-        </ul>
+        <div className="nav-wrapper">
+          <Link to='/' className="brand-logo center">
+            Invoice Generator
+          </Link>
+          <ul>
+            {/* <li><Link to='/'>Home</Link></li> */}
+            <li><Link to='/'>Generate Invoice</Link></li>
+            <li><Link to='/invoices'>View Invoice</Link></li>
+            <li><Link to='/clients'>Clients</Link></li>
+            <li><Link to='/products'>Products</Link></li>
+          </ul>
+        </div>
       </nav>
     </header>
   )
--- frontend/src/component/SideLinkMain.js
+++ frontend/src/component/SideLinkMain.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import Home from './Home/Home'
 import GenerateInvoice from './Invoice/GenerateInvoice'
 import Invoice from './Invoice/Invoices'
 import InvoiceDetails from './Invoice/InvoiceDetails'
@@ -13,8 +12,7 @@ const Main = () => (
     <main>
         <div className="container">
             <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route path='/generateInv' component={GenerateInvoice}/>
+            <Route exact path='/' component={GenerateInvoice}/>
             <Route path='/invoices' component={Invoice}/>
             <Route path='/invoiceDetails/:id' component={InvoiceDetails}/>
             <Route path='/clients' component={Client}/>
